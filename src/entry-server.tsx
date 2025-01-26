55c8466f1c1d7464209f8dcc0fb090ab20388d40
FuseTim <fusetim.log@gmx.com>
2025-01-26 12:06:03 +0100

    fix: SEO / Font display

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -13,6 +13,7 @@ export default createHandler(() => (
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
           <meta name="google-site-verification" content="ceqkKmtLXsTYqBnmXvfhOaUn3PPG8STrXfR3h70Yvcc" />
+          <meta name="description" content="Le lycée Saint-Louis, situé au cœur du Quartier Latin, est le seul lycée en France dédié exclusivement aux Classes Préparatoires aux Grandes Écoles. Il propose une préparation de qualité, notamment à travers la filière MP2I (Mathématiques, Physique, Informatique et Ingénierie), lancée en 2021. Cette formation innovante prépare les étudiants aux concours des plus grandes écoles d'ingénieurs." />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="MP2I Saint Louis" />
           <meta property="og:url" content="https://mp2i-saint-louis.github.io/" />
