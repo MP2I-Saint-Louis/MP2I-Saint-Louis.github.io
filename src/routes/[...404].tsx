d257ed535a55d61d2e17e0afec64feb95f5d520e
FuseTim <fusetim.log@gmx.com>
2025-01-26 11:19:26 +0100

    add: 404

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,19 +1,25 @@
 import { Title } from "@solidjs/meta";
 import { HttpStatusCode } from "@solidjs/start";
+import "./404.css";
 
 export default function NotFound() {
   return (
     <main>
-      <Title>Not Found</Title>
+      <Title>Page introuvable</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <section>
+        <h1>Page Introuvable</h1>
+        <div id="explanation">
+          <p>
+            La page que vous recherchez n'existe pas ou plus.
+          </p>
+          <p>
+            Nous vous invitons à retourner à la page d'accueil.
+          </p>
+        </div>
+
+        <a href="/">Retour à la page d'accueil</a>
+      </section>
     </main>
   );
 }
