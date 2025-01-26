ae07a20425860a0187cfb5d7eed74cf8a9bcbb9f
FuseTim <fusetim.log@gmx.com>
2025-01-26 10:57:06 +0100

    add: prepare for deployment on GHPages

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
+    ssr: true,
     server: {
       compatibilityDate: '2025-01-21',
       prerender: {
         crawlLinks: true
-      }
+      },
+      baseURL: process.env.BASE_PATH,
+      preset: "static"
     }
   });
