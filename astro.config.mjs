cf4494f684b9f325647bf10af686a18c1330a840
FuseTim <fusetim.log@gmx.com>
2025-05-16 20:39:34 +0200

    CI: make it works

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     scopedStyleStrategy: 'class',
+    site: "https://mp2i-saint-louis.github.io"
 });
